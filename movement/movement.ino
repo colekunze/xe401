8e35623f4047f4d25d56db0c2505e4ae30938516
colekunze <cole.kunze@usma.edu>
2017-08-31 15:09:09 -0400

    solving 4foot movement

--- movement/movement.ino
+++ movement/movement.ino
@@ -5,17 +5,23 @@ Servo motor1;
 Servo motor2;
 
 void setup() {
-  // put your setup code here, to run once:
   motor1.attach(10);
   motor2.attach(13);
+  
+//  for(int i = 30; i <= 90; i++) {
+//    motor1.write(i);
+//    motor2.write(i);
+//    delay(10);
+//  }
+  for(int i = 90; i >=30; i--) {
+    motor1.write(i);
+    motor2.write(i);
+    delay(28);
+  }
+  motor1.detach();
+  motor2.detach();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int pos = 0; pos <= 180; pos += 1) {
-    motor1.write(pos);
-    motor2.write(pos);  
-  }
+  
 }
-
-/*https://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/arduino-code*/
