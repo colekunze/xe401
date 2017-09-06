fe94692a4efa60459c8c5bfae5939c0bf4f1888e
stonepresto <eecs@eecss-MacBook-Pro.local>
2017-09-06 10:42:24 -0400

    cleaning up rotate

--- rotate/rotate.ino
+++ rotate/rotate.ino
@@ -4,25 +4,25 @@
 Servo motor1;
 Servo motor2;
 
-void setup() {
-  motor1.attach(10);
-  motor2.attach(13);
-  
-//  for(int i = 30; i <= 90; i++) {
-//    motor1.write(i);
-//    motor2.write(i);
-//    delay(10);
-//}
-  for(int i = 90; i >=30; i--) {
-    int j = 30+(90-i)
-    motor1.write(i);
-    motor2.write(j);
-    delay(28);
-  }
-  motor1.detach();
-  motor2.detach();
+//<rotateDelay>
+//PURPOSE: The purpose of this function is to take an integer representing the degrees the user desires the robot to turn
+//and return the amount of delay necessary for approximately that degree of rotation. The equation was derived through
+//collection of data and plotting the results.
+int rotateDelay(int deg) {
+  return (150+deg)/15;  
 }
 
-void loop() {
-  
+void setup() {
+ motor1.attach(10);
+ motor2.attach(13);
+
+  for(int i = 90; i >=30; i--) {
+    int j = 30+(90-i); //opposite direction
+    motor1.write(i);
+    motor2.write(j);
+    delay(rotateDelay(270));
+    }
+
+ motor1.detach();
+ motor2.detach();
 }
