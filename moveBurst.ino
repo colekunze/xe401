411d5e28020ac46ac8488e9f41c12c913f7bd2f7
colekunze <cole.kunze@usma.edu>
2017-09-06 11:31:50 -0400

    move forward in short bursts
    PROBLEM: cants to the right slightly

---
+++ moveBurst.ino
@@ -0,0 +1,57 @@
+#include <Servo.h>
+
+
+
+//globals
+
+Servo motor1;
+
+Servo motor2;
+
+int start = 90;
+int fin = 0;
+
+void setup() {
+
+  
+
+  
+
+//  for(int i = 30; i <= 90; i++) {
+
+//    motor1.write(i);
+
+//    motor2.write(i);
+
+//    delay(10);
+
+//  }
+
+  motor1.attach(10);
+
+  motor2.attach(13);
+
+  
+
+
+
+}
+
+
+
+void loop() {
+  
+for(int i = 90; i >=30; i--) {
+
+    motor1.write(i);
+
+    motor2.write(i);
+
+    delay(10);
+ } 
+
+}
+ 
+
+
+
