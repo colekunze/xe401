7ba1f97145fecaa812d08b073c7971b6001ad1d4
stonepresto <eecs@eecss-MacBook-Pro.local>
2017-09-06 11:00:01 -0400

    adding main algo file

---
+++ main/main.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+//globals
+Servo motor1;
+Servo motor2;
+
+//FUNCTION LIST:
+//rotateDelay()
+//movementDelay()
+//databaseExists()
+//initializeDB()
+//
+
+void setup() {
+  //attach motor1
+  //attach motor2
+  //setup ping1
+  //setup ping2
+  //setup ping3
+  //setup photoresistor
+  //setup infared
+  //if(!databaseExists):
+    //initializeDB
+  //else setupDB
+  
+}
+
+void loop() {
+  
+}
+
