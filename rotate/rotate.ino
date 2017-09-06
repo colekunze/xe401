304396f74998d5c10c20bbfd32af90369e8ff9cb
colekunze <cole.kunze@usma.edu>
2017-09-06 10:34:29 -0400

    rotate equation

--- rotate/rotate.ino
+++ rotate/rotate.ino
@@ -1,28 +1,50 @@
 #include <Servo.h>
 
+
+
 //globals
+
 Servo motor1;
+
 Servo motor2;
 
-void setup() {
-  motor1.attach(10);
-  motor2.attach(13);
-  
-//  for(int i = 30; i <= 90; i++) {
-//    motor1.write(i);
-//    motor2.write(i);
-//    delay(10);
-//}
-  for(int i = 90; i >=30; i--) {
-    int j = 30+(90-i)
-    motor1.write(i);
-    motor2.write(j);
-    delay(28);
-  }
-  motor1.detach();
-  motor2.detach();
-}
 
-void loop() {
+
+void setup() {
+
+  motor1.attach(10);
+
+  motor2.attach(13);
+
   
+
+//  for(int i = 30; i <= 90; i++) {
+
+//    motor1.write(i);
+
+//    motor2.write(i);
+
+//    delay(10);
+
+//}
+
+//deg = 2.1351(delay) - 12.162
+//(12.162+deg)/2.1351 = delay
+int deg = 270;
+int delae = (150+deg)/15;
+  for(int i = 90; i >=30; i--) {
+    int j = 30+(90-i);
+
+    motor1.write(i);
+
+    motor2.write(j);
+
+    delay(delae); //16=~90 degrees 22=~180 degrees
+
+  }
+
+  motor1.detach();
+
+  motor2.detach();
+
 }
